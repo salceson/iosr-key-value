169a0ac8688dfba439056f03369d231e056a87cb
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-11-21 16:29:15 +0100

    Merge changes

--- scripts/check_clients.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-COMPLETED=`docker-compose ps | grep "Exit 0" | wc -l`
-if [ ${COMPLETED} -ne 3 ] ; then
-    echo "Completed ${COMPLETED} but expected 3."
-    exit 1
-fi
-exit 0
